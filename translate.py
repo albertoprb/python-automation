a7cc16cfb0b4a86e5ddab0a30748a2d2bcd8795f
Alberto Barcelos <albertoprb@gmail.com>
2024-06-03 12:48:09 +0200

    Changing translation keyboard bindings

--- translate.py
+++ translate.py
@@ -3,7 +3,7 @@ import webbrowser
 from pynput import keyboard
 
 # Dictionary to keep track of the state of each key
-key_state = {"ctrl": False, "c_count": 0}
+key_state = {"ctrl": False, "c_pressed": False}
 
 
 def on_press(key):
@@ -11,25 +11,22 @@ def on_press(key):
         key_state["ctrl"] = True
     elif key == keyboard.KeyCode.from_char("c"):
         if key_state["ctrl"]:
-            key_state["c_count"] += 1
-            if key_state["c_count"] == 2:
-                print("Ctrl + C + C detected. Translating...")
-                open_deepl_translator()
-                key_state["c_count"] = 0  # Reset the count after action
+            key_state["c_pressed"] = True
+    elif key == keyboard.KeyCode.from_char("d"):
+        if key_state["ctrl"] and key_state["c_pressed"]:
+            open_deepl_translator()
+            key_state["c_pressed"] = False  # Reset the state after action
 
 
 def on_release(key):
     if key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
         key_state["ctrl"] = False
-    elif key == keyboard.KeyCode.from_char("c"):
-        if not key_state["ctrl"]:
-            key_state["c_count"] = (
-                0  # Reset the count if 'c' is released without 'ctrl'
-            )
+        key_state["c_pressed"] = False  # Reset the state if 'ctrl' is released
 
 
 # Function to open the URL with the clipboard content
 def open_deepl_translator():
+    print("Opening DeepL translator...")
     text = pyperclip.paste()
     url = f"https://www.deepl.com/translator#de/en/{text}"
     webbrowser.get("firefox")
@@ -37,7 +34,7 @@ def open_deepl_translator():
 
 
 if __name__ == "__main__":
-    print("Listening for Ctrl + C + C...")
+    print("Listening for Ctrl + C + D...")
     with keyboard.Listener(
         on_press=on_press,
         on_release=on_release
